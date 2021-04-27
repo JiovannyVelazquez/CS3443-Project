bb86cee13ec7084e0458041469198e7ec00196f0
ben <hueroacapulco@gmail.com>
2021-04-27 01:43:38 -0500

    adding new rooms

---
+++ CS3343-AquasProject/src/controller/ControllerRoom3.java
@@ -0,0 +1,28 @@
+package controller;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ControllerRoom3 {
+	
+	public void changeBackSceneButtonPushed(ActionEvent event) throws IOException {
+		
+		Parent sceneParent = FXMLLoader.load(getClass().getResource("/view/Room2.fxml"));
+		Scene sceneX = new Scene(sceneParent);
+		
+		//this line gets the stage information
+
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		window.setScene(sceneX);
+		window.show();
+
+	}
+
+
+}
