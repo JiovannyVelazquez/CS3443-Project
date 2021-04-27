26ada80cc5a78917618b48466bbd066441f0f3c1
Jiovanny Velazquez <rhv754@my.utsa.edu>
2021-04-27 15:39:09 -0500

    Added addItem and remove item functions for inventory

--- CS3343-AquasProject/src/model/Inventory.java
+++ CS3343-AquasProject/src/model/Inventory.java
@@ -1,5 +1,30 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Inventory {
-	///ADDcomment in inventory
+	private ArrayList<Item> inventory;
+	
+	
+	//Returns true or false depending if item 
+	//was accepted in the inventory (false if 
+	//inventory is full)
+	public boolean addItem(Item newItem) {
+		boolean notfull = true;
+		int invSize = inventory.size();
+		//If inventory full then don't add item
+		if(invSize == 2){
+			notfull = false;
+		}
+		else {
+			inventory.add(newItem);
+		}
+		return notfull;
+	}
+
+	//Returns true when item successfully removed
+	public void removeItem(Item itemGiven) {
+		inventory.remove(itemGiven);
+	}
+	
 }
